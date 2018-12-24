b84bb46d53ae35cf28f47592f09f36ae6e1e860f
lafitewu <863512936@qq.com>
2018-12-24 16:21:19 +0800

    重定向默认打开页面

--- routes/index.js
+++ routes/index.js
@@ -28,10 +28,11 @@ var responseJSON = function (res, ret) {
 };
 
 /* GET home page. */
-router.get('/yyt/cj', function(req, res, next) {
-	var result = {"baseResponse": { "code": 200, "msg": "成功" },"items":[{"type":"CJ","data":{"title": "跑团缺席","infro":"","resource": "幂动科技","detailUrl": "https://awtmt.com/articles/3364282", "images":["http://img.tuku.cn/file_thumb/201503/m2015031016500369.jpg","http://old.bz55.com/uploads/allimg/120804/1-120P4092145.jpg","http://b.hiphotos.baidu.com/zhidao/pic/item/9358d109b3de9c82f897b2246c81800a18d843ae.jpg"]}}]};
-	responseJSON(res,result);
-  // res.json();
+router.get('/', function(req, res, next) {
+	res.writeHead(301, {
+		'Location': 'https://www.lafitewu.xyz/'
+	  });
+	res.end()
 });
 //小程序分享api
 router.get('/wxTool/share/api',function(req,res,next) {
